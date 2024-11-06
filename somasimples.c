b49fc62d306c5d667b0dc049ec0518f434f149f5
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2024-11-06 18:29:14 -0300

    Create somasimples.c

---
+++ somasimples.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ // Exercício Beecrowd 1003
+
+int main() {
+ 
+   int A, B, soma;
+   
+   //Leitura das variáveis
+   scanf("%d %d", &A, &B);
+   
+   //Somatória
+   soma = A + B;
+   
+   //Saída
+   printf("SOMA = %d\n", soma);
+ 
+    return 0;
+}
