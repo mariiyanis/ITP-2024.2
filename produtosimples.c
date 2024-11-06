1fb960859021d9546b267c19dc56328ed3c015a9
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2024-11-06 18:35:58 -0300

    Create produtosimples.c

---
+++ produtosimples.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Exercício Beecrowd 1004 
+
+int main() {
+ 
+    int A, B, PROD;
+    // Leitura das variáveis
+    scanf("%d %d", &A, &B);
+    
+    //Multiplicação das variáveis
+    PROD = A * B;
+    
+    //Saída
+    printf("PROD = %d\n", PROD);
+ 
+    return 0;
+}
