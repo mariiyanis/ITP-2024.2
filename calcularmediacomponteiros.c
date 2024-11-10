1775ab86b44e21cfd128c774126951e368ff2a00
Marília Yanis <164898020+mariiyanis@users.noreply.github.com>
2024-11-10 11:18:30 -0300

    Create calcularmediacomponteiros.c

---
+++ calcularmediacomponteiros.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void calcular_media(int *A, int *B) {
+    int menor, maior;
+    
+    if (*A < *B) {
+        menor = *A;
+        maior = *B;
+    } else {
+        menor = *B;
+        maior = *A;
+    }
+
+    int media = (menor + maior) / 2;
+
+    if (*A < *B) {
+        *A = media;
+        *B = (menor + maior) % 2; 
+    } else {
+        *B = media;
+        *A = (menor + maior) % 2;
+    }
+}
+
+int main() {
+    int A, B;
+
+    scanf("%d %d", &A, &B);
+
+    calcular_media(&A, &B);
+
+
+    printf("A = %d\n", A);
+    printf("B = %d\n", B);
+
+    return 0;
+}
